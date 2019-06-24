2eadbf3ef86d2bbe9ba185cb2297eea2b5fe0a89
cyperghost <crcerror@users.noreply.github.com>
2019-06-24 19:30:58 +0200

    made arr call much simplier and faster

--- conf-parser/batoceraSettings.py
+++ conf-parser/batoceraSettings.py
@@ -5,5 +5,5 @@ import os
 
 scriptDir = os.path.dirname(os.path.realpath(__file__))
 sys.argv[0]="mimic_python"
-str1 = '|'.join(str(i) for i in sys.argv)
+str1 = '\n'.join(str(i) for i in sys.argv)
 os.system('bash "%s"/batoceraSettings.sh "%s"' % (scriptDir, str1))
