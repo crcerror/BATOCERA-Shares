1ef57a296d5d394e407884338262807d763ed857
cyperghost <crcerror@users.noreply.github.com>
2020-06-07 16:00:17 +0200

    Update wps_pushbutton.sh

--- wifi/wps_pushbutton.sh
+++ wifi/wps_pushbutton.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 # First attempt for adding WPS push button connection to BATOCERA
-#
+# This works now - 07.06.2020
 
-wifi_setupdir=/var/lib/connman
-wifi_conn="VALUE FROM LIST"
-
-#echo "$wifi_conn" | grep "VALUE" | awk -F "VALUE[ ]*" '{print $2}'
-#exit
 #### Show list
 batocera-wifi scanlist
 
@@ -14,14 +9,16 @@ read -p "Enter SSID to connect and push [WPS]-button: " wifi_conn
 connect_tp=$(connmanctl services | grep -m1 "$wifi_conn" | awk -F "$wifi_conn[ ]*" '{print $2}')
 connmanctl connect $connect_tp &> /dev/shm/wps.log
 
-if grep "^Connected" /dev/shm/wps.log; then
+if grep -q "^Connected" /dev/shm/wps.log; then
     echo "WPS connected!"
     echo "Connection established" >> /dev/shm/wps.log
     conn_cred="/var/lib/connman/$connect_tp/settings"
     ssid="$(batocera-settings "$conn_cred" get Name)"
     psk="$(batocera-settings "$conn_cred" get Passphrase)"
-    echo "SSID: $ssid" >> /dev/shm/wps.log
-    echo "PSK: $psk"   >> /dev/shm/wps.log
+    batocera-settings set wifi.ssid "$ssid"
+    batocera-settings set wifi.key "$psk"
+elif grep -q "Already connected" /dev/shm/wps.log; then
+    echo "Connections already established to '$wifi_conn'"
 else
     echo "Failed: WPS connection"
 fi
